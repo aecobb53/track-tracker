65b66151e11b26fdbae8f8fea99afeb5612a77c5
acobb <aecobb53@gmail.com>
2025-02-14 17:34:22 -0700

    updated data

--- data_currator.py
+++ data_currator.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import pyperclip
 
 
 event_rename = {
@@ -23,34 +24,70 @@ def rename_event(event):
 with open('deleteme.json', 'r') as jf:
     data = json.load(jf)
 
-# for k, v in data.items():
-#     if not v['raw']:
-#         print(k)
-
 athletes = {}
-# different_event_names = set()
+different_event_names = set()
 for meet, details in data.items():
     for event, results in details.items():
         if event == 'raw':
             continue
         for result in results:
             if 'Fairview High School' in result['team']:
-                athlete = result.get('athlete', 'relay')
+                athlete = result.get('athlete', 'Relay')
                 event = rename_event(result['event'])
                 result['meet'] = meet
                 if athlete not in athletes:
                     athletes[athlete] = {}
-                if result['event'] not in athletes[athlete]:
+                if event not in athletes[athlete]:
                     athletes[athlete][event] = []
                 athletes[athlete][event].append(result)
+                different_event_names.add(event)
+
+meets = {}
+for meet, details in data.items():
+    for event, results in details.items():
+        if event == 'raw':
+            continue
+        for result in results:
+            if 'Fairview High School' in result['team']:
+                athlete = result.get('athlete', 'Relay')
+                event = rename_event(result['event'])
+                result['meet'] = meet
+                if meet not in meets:
+                    meets[meet] = {}
+                if event not in meets[meet]:
+                    meets[meet][event] = []
+                meets[meet][event].append(result)
                 # different_event_names.add(event)
-                
+
 with open('deleteme2.json', 'w') as jf:
     jf.write(json.dumps(athletes, indent=4))
+with open('deleteme3.json', 'w') as jf:
+    jf.write(json.dumps(meets, indent=4))
 
 # a = list(different_event_names)
 # a.sort()
 # for i in a:
 #     print(i)
+
+csv = ['Name,Event,Mark,Wind,Attempt,Year,Meet']
+for athlete, events in athletes.items():
+    
+    if athlete == 'Relay':
+        continue
+    for event_name, results in events.items():
+        for result in results:
+            if result['mark'] is None:
+                x=1
+            data = [athlete]
+            data.append(event_name)
+            data.append(str(result['mark']))
+            data.append(str(result['wind']))
+            data.append(str(result['attempt']))
+            data.append(str(result['calendar_year']))
+            data.append(str(result['meet']))
+            row_s = f','.join(data)
+            row_s = row_s.replace('None', '-')
+            csv.append(row_s)
+pyperclip.copy('\n'.join(csv))
 
 x=1
--- meet_results_formatter.py
+++ meet_results_formatter.py
@@ -3,17 +3,27 @@ import re
 import json
 
 
-def parse_data_row(data_row: list[str], event: str, header: list[str], meet_name: str = None):
+def parse_data_row(data_row: list[str], event: str, header: list[str], calendar_year: int, meet_name: str):
     # New result indicator
     if len(data_row) == 5 and data_row[-1] == '':
         data_row.pop(-1)
-    if len(data_row) == 3:
-        # Missing video column
-        if data_row[1] != '':
-            data_row.insert(1, '')
+    # if len(data_row) == 3:
+    #     # Missing video column
+    #     if data_row[1] != '':
+    #         data_row.insert(1, '')
+
+    if not event:
+        return None
+    if 'decathlon' in event.lower():
+        # PARSE AT SOME POINT
+        return None
+    if 'heptathlon' in event.lower():
+        # PARSE AT SOME POINT
+        return None
+
     A_header_len = len(header)
     A_row_len = len(data_row)
-    if len(header) == 7 and len(data_row) == 4:
+    if len(header) == 7 and len(data_row) == 3:
         if header == ['PLACE', 'VIDEO', 'ATHLETE', 'TEAM', 'MARK', 'WIND', 'HEAT']:
             attempt = None
             year = None
@@ -23,22 +33,22 @@ def parse_data_row(data_row: list[str], event: str, header: list[str], meet_name
             wind = None
             heat = None
 
-            result_wind_re = re.search(r'[\s\t]+((\d+:)?\d+\.?\d*)[\s\t]+([-+])[\s\t]*(\d+.?\d*)[\s\t]+(\d+)', data_row[3])
-            result_no_wind_re = re.search(r'[\s\t]+((\d+:)?\d+?\.\d+)[\s\t]+(\d+)', data_row[3])
-            result_high_jump_re = re.search(r'[\s\t]+(\d+)-(\d+\.?\d*)[\s\t]+(\d+)', data_row[3])
-            result_long_jump_re = re.search(r'[\s\t]+(\d+)-(\d+\.?\d*)[\s\t]+([-+])(\d*\.?\d*)[\s\t]+(\d+)', data_row[3])
+            result_wind_re = re.search(r'[\s\t]+((\d+:)?\d+\.?\d*)[\s\t]+([-+])[\s\t]*(\d+.?\d*)[\s\t]+(\d+)', data_row[-1])
+            result_no_wind_re = re.search(r'[\s\t]+((\d+:)?\d+?\.\d+)[\s\t]+(\d+)', data_row[-1])
+            result_high_jump_re = re.search(r'[\s\t]+(\d+)-(\d+\.?\d*)[\s\t]+(\d+)', data_row[-1])
+            result_long_jump_re = re.search(r'[\s\t]+(\d+)-(\d+\.?\d*)[\s\t]+([-+])(\d*\.?\d*)[\s\t]+(\d+)', data_row[-1])
             if result_wind_re:
                 mark, _, wind_sign, wind, heat = result_wind_re.groups()
-                team = data_row[3][:result_wind_re.start(0)].strip()
+                team = data_row[-1][:result_wind_re.start(0)].strip()
             elif result_no_wind_re:
                 mark, _, heat = result_no_wind_re.groups()
-                team = data_row[3][:result_no_wind_re.start(0)].strip()
+                team = data_row[-1][:result_no_wind_re.start(0)].strip()
             elif result_high_jump_re:
                 attempt, mark, heat = result_high_jump_re.groups()
-                team = data_row[3][:result_high_jump_re.start(0)].strip()
+                team = data_row[-1][:result_high_jump_re.start(0)].strip()
             elif result_long_jump_re:
                 attempt, mark, wind_sign, wind, heat = result_long_jump_re.groups()
-                team = data_row[3][:result_long_jump_re.start(0)].strip()
+                team = data_row[-1][:result_long_jump_re.start(0)].strip()
             else:
                 x=1
 
@@ -59,8 +69,9 @@ def parse_data_row(data_row: list[str], event: str, header: list[str], meet_name
 
             data_obj = {
                 'place': int(data_row[0]),
-                'athlete': data_row[2],
+                'athlete': data_row[1],
                 'year': year,
+                'calendar_year': calendar_year,
                 'team': team,
                 'attempt': attempt,
                 'mark': mark,
@@ -70,12 +81,12 @@ def parse_data_row(data_row: list[str], event: str, header: list[str], meet_name
                 'event': event
             }
             return data_obj
-    elif len(header) == 5 and len(data_row) == 4:
+    elif len(header) == 5 and len(data_row) == 3:
         if header == ['PLACE', 'VIDEO', 'TEAM', 'MARK', 'HEAT']:
             mark = None
             heat = None
 
-            result_relay_re = re.search(r'((\d+:)?\d+\.?\d*)[\s\t]+(\d+)', data_row[3])
+            result_relay_re = re.search(r'((\d+:)?\d+\.?\d*)[\s\t]+(\d+)', data_row[-1])
             if result_relay_re:
                 mark, _, heat = result_relay_re.groups()
             else:
@@ -105,9 +116,7 @@ def parse_meet_file(path: str):
     with open(path, 'r') as tf:
         data = [l[:-1] for l in tf.readlines()]
     meet_name = os.path.splitext(os.path.basename(path))[0]
-
-    if meet_name in ['ATB Churchyard PV Meet', 'Arcadia Invitational']:
-        x=1
+    calendar_year = int(path[:4])
 
     event = None
     header = []
@@ -115,11 +124,13 @@ def parse_meet_file(path: str):
     data_row = []
     # for line in data:
     for index, line in enumerate(data):
-        if meet_name == 'Colorado State Track and Field Championships' and index == 14976:
-            x=1
+        # if meet_name == 'Arcadia Invitational' and calendar_year == 2022 and index == 4510:
+        #     x=1
+        if 'All Results' in line or not line:
+            continue
         newline_re = re.search(r'^\d+$', line.strip())
         if newline_re:
-            row = parse_data_row(data_row=data_row, event=event, header=header, meet_name=meet_name)
+            row = parse_data_row(data_row=data_row, event=event, header=header, calendar_year=calendar_year, meet_name=meet_name)
             if row:
                 meet_data.append(row)
             data_row = []
@@ -137,20 +148,20 @@ def parse_meet_file(path: str):
             ' mens? ',
             ' womens? ',
             )
-        if any([re.search(i, line.replace("'", '').strip().lower()) for i in re_strings]):
+        re_skip_strings = (
+            'girls? academic',
+        )
+        if any([re.search(i, line.replace("'", '').strip().lower()) for i in re_strings]) and not any([re.search(i, line.replace("'", '').strip().lower()) for i in re_skip_strings]):
             if data_row:
                 data_row.pop(-1)
 
-            row = parse_data_row(data_row=data_row, event=event, header=header)
+            row = parse_data_row(data_row=data_row, event=event, header=header, calendar_year=calendar_year, meet_name=meet_name)
             if row:
                 meet_data.append(row)
             data_row = []
             event = line
         if line.strip().lower().startswith(('place')):
             header = [l.strip() for l in line.split(' ')]
-
-    if meet_name in ['Fairview JV Invite', 'ATB Churchyard PV Meet', 'Arcadia Invitational']:
-        x=1
 
     return_object = {}
     for record in meet_data:
